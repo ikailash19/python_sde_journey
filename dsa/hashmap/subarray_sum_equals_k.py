9b84e55852c5fe34619e9783c11031706d53c2cd
kailash <kailashiviswanathan@gmail.com>
2026-04-25 20:07:45 +0530

    DSA: longest substring without repeating characters (sliding window pattern)

--- dsa/hashmap/subarray_sum_equals_k.py
+++ dsa/hashmap/subarray_sum_equals_k.py
@@ -9,3 +9,41 @@ def subarray_sum(nums, k):
     return total
 
 print(subarray_sum([1,1,1], 2))
+
+def subarray_sum_optimized(nums, k):
+    subarray_sums = {0:1}
+    current_sum = 0
+    count = 0
+    for i in range (len(nums)):
+        current_sum += nums[i]
+        if current_sum - k in subarray_sums:
+            count += subarray_sums[current_sum - k]
+        subarray_sums[current_sum] = subarray_sums.get(current_sum, 0) + 1
+    return count
+print(subarray_sum_optimized([1,2,3,3,6,9],3))
+
+def subarray_sum_optimized_v2(nums, k):
+    subarray_sums = {0:1}
+    count = 0
+    current_sum = 0
+    for i in range (len(nums)):
+        current_sum += nums[i]
+        if current_sum - k in subarray_sums:
+            count += subarray_sums[current_sum - k]
+        subarray_sums[current_sum] = subarray_sums.get(current_sum, 0) + 1
+    return count
+print(subarray_sum_optimized_v2([1,2,3,3,6,9], 3))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
